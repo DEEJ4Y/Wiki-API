0a0636355672411b7bab50a0a6d499c56baaaa92
davidjosephind <davidjosephind@gmail.com>
2020-12-28 17:23:22 +0530

    Add API get all articles

--- app.js
+++ app.js
@@ -8,15 +8,27 @@ mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useUnifiedTopology: true,
 });
 
-const articlesSchema = new mongoose.Schema({
+const articleSchema = new mongoose.Schema({
   title: String,
   content: String,
 });
+
+const Article = new mongoose.model("Article", articleSchema);
 
 const app = express();
 
 app.get("/", function (req, res) {
   res.send("<h1>We are connected!</h1>");
+});
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, articles) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(articles);
+    }
+  });
 });
 
 app.listen(3000, function () {
