bf228f50323cb768935b2f3a24dfcfff9e60684a
davidjosephind <davidjosephind@gmail.com>
2020-12-28 19:25:10 +0530

    Add chained route handlers

--- app.js
+++ app.js
@@ -23,40 +23,40 @@ app.get("/", function (req, res) {
   res.send("<h1>We are connected!</h1>");
 });
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, articles) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(articles);
-    }
-  });
-});
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, articles) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(articles);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
-app.post("/articles", function (req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully uploaded article!");
+      } else {
+        res.send("Oops! Something went wrong...<br>" + err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles!");
+      } else {
+        res.send("Oops! Something went wrong...<br>" + err);
+      }
+    });
   });
-
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send("Successfully uploaded article!");
-    } else {
-      res.send("Oops! Something went wrong...<br>" + err);
-    }
-  });
-});
-
-app.delete("/articles", function (req, res) {
-  Article.deleteMany(function (err) {
-    if (!err) {
-      res.send("Successfully deleted all articles!");
-    } else {
-      res.send("Oops! Something went wrong...<br>" + err);
-    }
-  });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
